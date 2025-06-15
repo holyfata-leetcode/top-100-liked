1b40aebafaa86b5053e35d005a61bfb120726e77
holyfata <holyfata@qq.com>
2025-06-15 16:55:30 +0800

    feat: 更新update_progress脚本

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 <!-- update_progress -->
 
-  ![](https://img.shields.io/badge/编程语言-Rust-dea584)
+![](https://img.shields.io/badge/编程语言-Rust-dea584)
 
-  ![](https://img.shields.io/badge/进度-2%25-blue)
+![](https://img.shields.io/badge/进度-2%25-blue)
 
-  <!-- update_progress -->
+<!-- update_progress -->
--- scripts/update_progress.ts
+++ scripts/update_progress.ts
@@ -21,9 +21,9 @@ let readme = fs.readFileSync(readmePath, 'utf-8');
 readme = readme.replace(
   /(<!-- update_progress -->)[\s\S]*?(<!-- update_progress -->)/,
   `<!-- update_progress -->\n
-  ![](https://img.shields.io/badge/编程语言-Rust-dea584)\n
-  ![](https://img.shields.io/badge/进度-${percent}%25-blue)\n
-  <!-- update_progress -->`
+![](https://img.shields.io/badge/编程语言-Rust-dea584)\n
+![](https://img.shields.io/badge/进度-${percent}%25-blue)\n
+<!-- update_progress -->`
 );
 
 // 写回 README.md
